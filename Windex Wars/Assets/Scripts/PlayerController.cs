ef2e099f6fa820f3f8fab47a4e4bad3eaa13cb8d
Surya <suryan993@gmail.com>
2020-03-02 17:58:57 -0500

    added all keys for controller 4 players

--- Windex Wars/Assets/Scripts/PlayerController.cs
+++ Windex Wars/Assets/Scripts/PlayerController.cs
@@ -29,8 +29,18 @@ public class PlayerController : MonoBehaviour
         float ver = Input.GetAxis("JoystickLeftStickVerticalPlayer"+PlayerNumber);
         float hor1 = Input.GetAxis("JoystickRightStickHorizontalPlayer" + PlayerNumber);
         float ver1 = Input.GetAxis("JoystickRightStickVerticalPlayer" + PlayerNumber);
+        float hor2 = Input.GetAxis("JoystickDPadHorizontalPlayer" + PlayerNumber);
+        float ver2 = Input.GetAxis("JoystickDPadVerticalPlayer" + PlayerNumber);
+        float lt = Input.GetAxis("JoystickLTPlayer" + PlayerNumber);
+        float rt = Input.GetAxis("JoystickRTPlayer" + PlayerNumber);
         bool a = Input.GetButton("JoystickAPlayer"+PlayerNumber);
         bool b = Input.GetButton("JoystickBPlayer" + PlayerNumber);
+        bool x = Input.GetButton("JoystickXPlayer" + PlayerNumber);
+        bool y = Input.GetButton("JoystickYPlayer" + PlayerNumber);
+        bool lb = Input.GetButton("JoystickLBPlayer" + PlayerNumber);
+        bool rb = Input.GetButton("JoystickRBPlayer" + PlayerNumber);
+        bool select = Input.GetButton("JoystickSelectPlayer" + PlayerNumber);
+        bool start = Input.GetButton("JoystickStartPlayer" + PlayerNumber);
         if (a)
         {
             Debug.Log("A - Player"+PlayerNumber);
@@ -39,8 +49,34 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("B - Player" + PlayerNumber);
         }
-        Debug.Log("Left Stick Hor - "+hor+" Ver"+ver +" Player" + PlayerNumber);
-        Debug.Log("Right Stick Hor - " + hor1 + " Ver" + ver1 + " Player" + PlayerNumber);
+        if (x)
+        {
+            Debug.Log("X - Player" + PlayerNumber);
+        }
+        if (y)
+        {
+            Debug.Log("Y - Player" + PlayerNumber);
+        }
+        if (start)
+        {
+            Debug.Log("Start - Player" + PlayerNumber);
+        }
+        if (select)
+        {
+            Debug.Log("Select - Player" + PlayerNumber);
+        }
+        if (lb)
+        {
+            Debug.Log("LB - Player" + PlayerNumber);
+        }
+        if (rb)
+        {
+            Debug.Log("RB - Player" + PlayerNumber);
+        }
+        Debug.Log("Left Stick Hor - "+hor+" Ver - "+ver +" Player" + PlayerNumber);
+        Debug.Log("Right Stick Hor - " + hor1 + " Ver - " + ver1 + " Player" + PlayerNumber);
+        Debug.Log("Dpad hor - " + hor2 + " Ver - " + ver2 + " Player" + PlayerNumber);
+        Debug.Log("LT - " + lt + " RT - " + rt + " Player" + PlayerNumber);
         Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
         transform.Translate(playerMovement, Space.Self);
 
