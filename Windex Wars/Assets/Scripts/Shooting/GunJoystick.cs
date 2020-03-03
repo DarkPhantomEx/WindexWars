3b155ef5a3286672e030237ee19286751c91659c
Surya <suryan993@gmail.com>
2020-03-03 05:10:30 -0500

    added firing to joystick

---
+++ Windex Wars/Assets/Scripts/Shooting/GunJoystick.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunJoystick : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+    public GameObject player;
+    public float offset;
+
+    public GameObject bulletPerfab;
+
+    public int PlayerNumber;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool b = Input.GetButton("JoystickBPlayer" + PlayerNumber);
+        if (b)
+        {
+            //Shoot();
+            GameObject bulletObject = Instantiate(bulletPerfab);
+            bulletObject.transform.position = this.transform.position+ transform.forward;
+            bulletObject.transform.forward = this.transform.forward;
+        }
+    }
+
+    /*void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(player.transform.position, player.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }*/
+
+
+
+
+
+}
