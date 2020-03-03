fca13a7b1f0169bf45e742e91395b2bffe02779e
Lanzarote_ZRY <33143405+LanzaroteZRY@users.noreply.github.com>
2020-03-03 04:47:11 -0500

    Implement in splitscreenV2

---
+++ Windex Wars/Assets/Scripts/Shooting/Gun3.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun3 : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+    public GameObject player;
+    public float offset;
+
+    public GameObject bulletPerfab;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            //Shoot();
+            GameObject bulletObject = Instantiate(bulletPerfab);
+            bulletObject.transform.position = this.transform.position + transform.forward;
+            bulletObject.transform.forward = this.transform.forward;
+        }
+    }
+
+    /*void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(player.transform.position, player.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }*/
+
+
+
+
+
+}
